d1dd6b9a57317f3af5d0d3079e6c29151fde196a
Albert~M <105120358+Master-Chief131@users.noreply.github.com>
2025-06-13 08:58:45 -0500

    Restore InvwebServicioController functionality and re-enable API endpoint for fetching active services

--- src/main/java/com/example/demo/controller/InvwebServicioController.java
+++ src/main/java/com/example/demo/controller/InvwebServicioController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-//@RestController
-//@RequestMapping("/api/servicios")
-//public class InvwebServicioController {
-//     @Autowired
-//     private InvwebServicioRepository servicioRepository;
-//
-//     @GetMapping
-//     public List<InvwebServicio> getServicios() {
-//         return servicioRepository.findActivosConFamiliaVerPortal("S");
-//     }
-// }
+@RestController
+@RequestMapping("/api/servicios")
+public class InvwebServicioController {
+    @Autowired
+    private InvwebServicioRepository servicioRepository;
+
+    @GetMapping
+    public List<InvwebServicio> getServicios() {
+        return servicioRepository.findActivosConFamiliaVerPortal("S");
+    }
+}

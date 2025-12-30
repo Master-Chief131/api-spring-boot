743cbe99b5c6f7a3b84ad45610939819fd4006ae
Albert~M <luis.marquez3@utp.ac.pa>
2025-12-30 12:24:40 -0500

    feat: Add `InvwebExistenciaRepository` with custom queries.

--- src/main/java/com/example/demo/repository/InvwebExistenciaRepository.java
+++ src/main/java/com/example/demo/repository/InvwebExistenciaRepository.java
@@ -9,30 +9,33 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface InvwebExistenciaRepository extends JpaRepository<InvwebExistencia, InvwebExistenciaId> {
-    @Query("SELECT e FROM InvwebExistencia e WHERE EXISTS (SELECT 1 FROM InvwebBodega b WHERE b.noCia = e.noCia AND b.noSucursal = e.noSucursal AND b.noBodega = e.noBodega AND b.verPortal = 'S')")
-    Page<InvwebExistencia> findExistenciasConBodegaVerPortal(Pageable pageable);
-    
-    @Query("SELECT e FROM InvwebExistencia e " +
-           "INNER JOIN InvwebArticulo a ON a.noCia = e.noCia AND a.noArticulo = e.noArticulo " +
-           "INNER JOIN InvwebFamilia f ON f.noCia = a.noCia AND f.noFamilia = a.noFamilia " +
-           "INNER JOIN InvwebSubfamilia sf ON sf.noCia = a.noCia AND sf.noFamilia = a.noFamilia AND sf.noSubfamilia = a.noSubFamilia " +
-           "WHERE e.cantidad IS NOT NULL AND e.cantidad > 0 " +
-           "AND f.verPortal = 'S' " +
-           "AND sf.activo = 'S' " +
-           "AND a.verPortal = 'S' " +
-           "AND a.activo = 'S' " +
-           "AND EXISTS (SELECT 1 FROM InvwebBodega b WHERE b.noCia = e.noCia AND b.noSucursal = e.noSucursal AND b.noBodega = e.noBodega AND b.verPortal = 'S')")
-    Page<InvwebExistencia> findExistenciasConStockEnBodegasPortal(Pageable pageable);
-    
-    @Query("SELECT e FROM InvwebExistencia e " +
-           "INNER JOIN InvwebArticulo a ON a.noCia = e.noCia AND a.noArticulo = e.noArticulo " +
-           "INNER JOIN InvwebFamilia f ON f.noCia = a.noCia AND f.noFamilia = a.noFamilia " +
-           "INNER JOIN InvwebSubfamilia sf ON sf.noCia = a.noCia AND sf.noFamilia = a.noFamilia AND sf.noSubfamilia = a.noSubFamilia " +
-           "WHERE e.cantidad IS NOT NULL AND e.cantidad > 0 " +
-           "AND f.verPortal = 'S' " +
-           "AND sf.activo = 'S' " +
-           "AND a.verPortal = 'S' " +
-           "AND a.activo = 'S' " +
-           "AND EXISTS (SELECT 1 FROM InvwebBodega b WHERE b.noCia = e.noCia AND b.noSucursal = e.noSucursal AND b.noBodega = e.noBodega AND b.verPortal = 'S' AND b.noCia = :noCia)")
-    Page<InvwebExistencia> findExistenciasConStockEnBodegasPortalPorCia(@Param("noCia") Integer noCia, Pageable pageable);
+       @Query("SELECT e FROM InvwebExistencia e WHERE EXISTS (SELECT 1 FROM InvwebBodega b WHERE b.noCia = e.noCia AND b.noSucursal = e.noSucursal AND b.noBodega = e.noBodega AND b.verPortal = 'S')")
+       Page<InvwebExistencia> findExistenciasConBodegaVerPortal(Pageable pageable);
+
+       @Query("SELECT e FROM InvwebExistencia e " +
+                     "INNER JOIN InvwebArticulo a ON a.noCia = e.noCia AND a.noArticulo = e.noArticulo " +
+                     "INNER JOIN InvwebFamilia f ON f.noCia = a.noCia AND f.noFamilia = a.noFamilia " +
+                     "INNER JOIN InvwebSubfamilia sf ON sf.noCia = a.noCia AND sf.noFamilia = a.noFamilia AND sf.noSubfamilia = a.noSubFamilia "
+                     +
+                     "WHERE e.cantidad IS NOT NULL AND e.cantidad > 0 " +
+                     "AND sf.activo = 'S' " +
+                     "AND a.activo = 'S' " +
+                     "AND a.verPortal != 'O' " +
+                     "AND (a.verPortal = 'S' OR (a.verPortal = 'N' AND f.verPortal = 'S')) " +
+                     "AND EXISTS (SELECT 1 FROM InvwebBodega b WHERE b.noCia = e.noCia AND b.noSucursal = e.noSucursal AND b.noBodega = e.noBodega AND b.verPortal = 'S')")
+       Page<InvwebExistencia> findExistenciasConStockEnBodegasPortal(Pageable pageable);
+
+       @Query("SELECT e FROM InvwebExistencia e " +
+                     "INNER JOIN InvwebArticulo a ON a.noCia = e.noCia AND a.noArticulo = e.noArticulo " +
+                     "INNER JOIN InvwebFamilia f ON f.noCia = a.noCia AND f.noFamilia = a.noFamilia " +
+                     "INNER JOIN InvwebSubfamilia sf ON sf.noCia = a.noCia AND sf.noFamilia = a.noFamilia AND sf.noSubfamilia = a.noSubFamilia "
+                     +
+                     "WHERE e.cantidad IS NOT NULL AND e.cantidad > 0 " +
+                     "AND sf.activo = 'S' " +
+                     "AND a.activo = 'S' " +
+                     "AND a.verPortal != 'O' " +
+                     "AND (a.verPortal = 'S' OR (a.verPortal = 'N' AND f.verPortal = 'S')) " +
+                     "AND EXISTS (SELECT 1 FROM InvwebBodega b WHERE b.noCia = e.noCia AND b.noSucursal = e.noSucursal AND b.noBodega = e.noBodega AND b.verPortal = 'S' AND b.noCia = :noCia)")
+       Page<InvwebExistencia> findExistenciasConStockEnBodegasPortalPorCia(@Param("noCia") Integer noCia,
+                     Pageable pageable);
 }
